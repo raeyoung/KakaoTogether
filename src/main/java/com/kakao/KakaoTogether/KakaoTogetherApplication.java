8b37e145cba609cbf356231b437b9c81ed4ce4b1
raeyoung <raeyoung233@gmail.com>
2022-04-20 16:51:51 +0900

    Create Project

---
+++ src/main/java/com/kakao/KakaoTogether/KakaoTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.KakaoTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/KakaoTogether/KakaoTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.KakaoTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
