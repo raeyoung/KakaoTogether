53c971bd57bc6d2044dd96bcc6b920626209ba92
raeyoung <raeyoung233@gmail.com>
2022-04-20 17:55:24 +0900

    Add thymeleaf dependency / create main.html

---
+++ src/main/java/com/kakao/KakaoTogether/controller/fund/MainController.java
@@ -0,0 +1,10 @@
+package com.kakao.KakaoTogether.controller.fund;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+
+}
